636d3d40c0429b9d00be1281d96cbed75f89f487
Muhamad Akbar Imron <akbarimrons@gmail.com>
2026-05-26 21:09:55 +0700

    feat: add favicon icon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -86,6 +86,11 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
+      {
+        rel: "icon",
+        type: "image/svg+xml",
+        href: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="%239333ea" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 10v6M2 10l10-5 10 5-10 5z"/><path d="M6 12v5c3 3 9 3 12 0v-5"/></svg>',
+      },
     ],
   }),
   shellComponent: RootShell,
